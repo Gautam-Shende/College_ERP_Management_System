c9d8c13dd61670db7cc168b6c4a0cfbb9750a81c
Gautam <gautamshende144@gmail.com>
2026-08-10 22:30:41 +0530

    cors added

--- server/server.js
+++ server/server.js
@@ -9,14 +9,14 @@ const departmentRoutes = require("./routes/departmentRoutes")
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin:process.env.CLIENT_URL,
-//     credentials: true,
-//   }),
-// );
+app.use(
+  cors({
+    origin:process.env.CLIENT_URL,
+    credentials: true,
+  }),
+);
 
-app.use(cors());
+// app.use(cors());
 
 app.use(express.json());
 
