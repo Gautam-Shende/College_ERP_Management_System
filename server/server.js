5a250b3862e309aaffa8bbff56549030034ed041
Gautam <gautamshende144@gmail.com>
2026-08-10 22:47:58 +0530

    issue

--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ const app = express();
 
 app.use(
   cors({
-    origin:process.env.CLIENT_URL,
+    // origin:process.env.CLIENT_URL,
+    origin:"https://college-erp-management-system-4d6j-three.vercel.app",
     credentials: true,
   }),
 );
