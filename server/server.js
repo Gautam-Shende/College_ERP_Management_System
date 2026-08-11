a9a51c3d1d385181f8e0c2bbf0166d8b239d54ed
Gautam <gautamshende144@gmail.com>
2026-08-11 08:13:37 +0530

    cors issue fixing.

--- server/server.js
+++ server/server.js
@@ -12,24 +12,27 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "https://college-erp-management-system-six.vercel.app",
+  "https://college-erp-management-system-git-main-gautam-shendes-projects.vercel.app/"
+];
+
 app.use(
   cors({
-    origin:process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl) 
+      // or if the origin is in the allowed list
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-     methods: [
-      "GET",
-      "POST",
-      "PUT",
-      "PATCH",
-      "DELETE",
-      "OPTIONS",
-    ],
-
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-    ],
-  }),
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
 );
 
 // app.use(cors());
