8feba74fa22cdaad8fc06665c4c4445981b0a5e7
Gautam <gautamshende144@gmail.com>
2026-08-17 11:36:05 +0530

    cors handled.

--- server/src/app.js
+++ server/src/app.js
@@ -17,27 +17,13 @@ const app = express();
 
 app.use(express.json());
 
-const allowedOrigins = [
-  process.env.CLIENT_URL,
-].filter(Boolean);
-
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Postman, curl, server-to-server requests
-      if (!origin) {
-        return callback(null, true);
-      }
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
+
 
 app.use(logger);
 
